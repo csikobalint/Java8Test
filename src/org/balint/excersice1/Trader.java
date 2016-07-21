43c3c7e408dc729db31f7a84f1ee9f8bc5015a0e
Balint <balint.csiko@gmail.com>
2016-07-21 22:33:02 +0200

    Initial commit

---
+++ src/org/balint/excersice1/Trader.java
@@ -0,0 +1,30 @@
+package org.balint.excersice1;
+
+/**
+ * Created by Bálint on 2016. 07. 21..
+ */
+public class Trader {
+    private final String name;
+    private final String city;
+
+    public Trader(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public String toString() {
+        return "Trader{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
